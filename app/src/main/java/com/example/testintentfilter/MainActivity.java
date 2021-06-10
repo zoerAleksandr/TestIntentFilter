3a29ba61595f7747e911ae26f701875212a6770a
Aleksandr <zoer.aleksandr@gmail.com>
2021-06-10 19:58:25 +0300

    homework#5

--- app/src/main/java/com/example/testintentfilter/MainActivity.java
+++ app/src/main/java/com/example/testintentfilter/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.testintentfilter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("calc://go_to_calculator");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                intent.putExtra("msg", "this app open from other app");
+                startActivity(Intent.createChooser(intent, null));
+            }
+        });
     }
 }
